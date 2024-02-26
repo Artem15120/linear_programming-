7a2f4f1d386811ba354f359928e0591b36c51201
Artem15120 <ar.mir2013@yandex.ru>
2024-02-26 20:34:51 +0300

    Add project files.

---
+++ WpfApp1/ApplicationViewModel.cs
@@ -0,0 +1,167 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media.Effects;
+using System.Windows.Media.Media3D;
+using Microsoft.Win32;
+using System.IO;
+
+//using CommunityToolkit.Mvvm.ComponentModel;
+using LiveChartsCore;
+using LiveChartsCore.SkiaSharpView;
+using LiveChartsCore.SkiaSharpView.Painting;
+using LiveChartsCore.SkiaSharpView.Painting.Effects;
+using LiveChartsCore.SkiaSharpView.VisualElements;
+using SkiaSharp;
+using LiveChartsCore;
+using LiveChartsCore.Defaults;
+using LiveChartsCore.SkiaSharpView;
+using System.Globalization;
+using System.Windows.Data;
+using LiveChartsCore.Geo;
+
+namespace WpfApp1
+{
+    public class ApplicationViewModel : INotifyPropertyChanged
+    {
+        public GAUS resGaus = new GAUS();
+        public ObservableCollection<ObservablePoint> test1LineData = new ObservableCollection<ObservablePoint>();
+        public ObservableCollection<ObservablePoint> test1PointData = new ObservableCollection<ObservablePoint>();
+        private RelayCommand test1;
+        private RelayCommand test2;
+        public RelayCommand Test1
+        {
+            get{
+                return test1 ??
+                  (test1 = new RelayCommand(obj =>
+                  {
+                      // Создание экземпляра OpenFileDialog
+                      OpenFileDialog openFileDialog = new OpenFileDialog();
+                      // Установка фильтра для выбора только текстовых файлов
+                      openFileDialog.Filter = "Text files (*.txt)|*.txt";
+                      // Открытие диалогового окна выбора файла
+                      bool? result = openFileDialog.ShowDialog();
+                      // Проверка, был ли выбран файл
+                      if (result == true)
+                      {
+                          // Получение пути к выбранному файлу
+                          string filePath = openFileDialog.FileName;
+                          GausLine = File.ReadAllLines(filePath);
+                      }
+                  }));
+            }
+        }
+        public RelayCommand Test2
+        {
+            get
+            {
+                return test2 ??
+                  (test2 = new RelayCommand(obj =>
+                  {
+                      string x = GausLine[0];
+                      string[] parts = x.Split(' ');
+
+                      if (!(int.TryParse(parts[0], out int firstNumber)) || !(int.TryParse(parts[1], out int secondNumber)))
+                      {
+                          MessageBox.Show("ERROR SYMBOL IN FIRST LINE");
+                          return;
+                      }
+
+                      double[][] newMas = new double[firstNumber][];
+                      for (int i = 0; i < firstNumber; i++)
+                      {
+                          string[] numbers = GausLine[i+1].Split(' ');
+                          newMas[i] = new double[secondNumber];
+
+                          for (int j = 0; j < secondNumber; j++)
+                          {
+                              double.TryParse(numbers[j], out newMas[i][j]);
+                          }
+                      }
+                      List<string> tmp = resGaus.GetInfo(newMas);
+                      GausLine = GausLine.Concat(tmp.Select(n => n.ToString())).ToArray();
+                  }));
+            }
+        }
+
+        private string[] gausLine;
+        public string[] GausLine
+        {
+            get { return gausLine; }
+            set
+            {
+                gausLine = value;
+                OnPropertyChanged(nameof(GausLine));
+            }
+        }
+
+        public ObservableCollection<ISeries> Series { get; set; }
+        public LabelVisual Title { get; set; } =
+            new LabelVisual
+            {
+                Text = "My chart title",
+                TextSize = 25,
+                Padding = new LiveChartsCore.Drawing.Padding(15),
+                Paint = new SolidColorPaint(SKColors.DarkSlateGray)
+            };
+
+        public ApplicationViewModel()
+        {
+            GausLine = new string[] { "3 4", "3 2 -5 -1", "2 -1 3 13", "1 2 -1 9" };
+
+            test1PointData.Add(new ObservablePoint(1.5, 1.5));
+
+            var strokeThickness = 10;
+            var strokeDashArray = new float[] { 3 * strokeThickness, 2 * strokeThickness };
+            var effect = new DashEffect(strokeDashArray);
+            Series = new ObservableCollection<ISeries>
+            {
+                new LineSeries<ObservablePoint>
+                {
+                    Values = test1LineData,
+                },
+                new LineSeries<ObservablePoint>
+                {
+                    Values = test1PointData,
+                    Stroke = new SolidColorPaint
+                    {
+                        Color = SKColors.Green,
+                        StrokeCap = SKStrokeCap.Round,
+                        StrokeThickness = strokeThickness,
+                        PathEffect = effect
+                    },
+                    Fill = null
+                }
+            };
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+    public class ArrayToStringConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is string[] array)
+            {
+                return string.Join("\n", array.Skip(1));
+            }
+            return value;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ WpfApp1/GAUS.cs
@@ -0,0 +1,199 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace WpfApp1
+{
+    public class GAUS
+    {
+        public GAUS() { }
+        //public string[] GetInfo(double[][] matrix)
+        //{
+        //    int n = matrix.Length;
+        //    for (int i = 0; i < n; i++)
+        //    {
+        //        for (int j = i + 1; j < n; j++)
+        //        {
+        //            double factor = matrix[j][i] / matrix[i][i];
+        //            for (int k = i; k < n + 1; k++)
+        //            {
+        //                matrix[j][k] -= factor * matrix[i][k];
+        //            }
+        //        }
+        //    }
+        //    double[] result = new double[n];
+        //    for (int i = n - 1; i >= 0; i--)
+        //    {
+        //        result[i] = matrix[i][n] / matrix[i][i];
+        //        for (int j = i - 1; j >= 0; j--)
+        //        {
+        //            matrix[j][n] -= matrix[j][i] * result[i];
+        //        }
+        //    }
+
+        //    string[] solutions = new string[n];
+        //    for (int i = 0; i < n; i++)
+        //    {
+        //        solutions[i] = $"x{i + 1} = {result[i]:F2}";
+        //    }
+
+        //    return solutions;
+        //}
+
+        public string[] GetInfo1(double[][] matrix)
+        {
+            int n = matrix.Length;
+            string[] result = new string[n];
+            double[][] modifiedMatrix = new double[n][];
+            for (int i = 0; i < n; i++)
+            {
+                modifiedMatrix[i] = new double[matrix[i].Length];
+                matrix[i].CopyTo(modifiedMatrix[i], 0);
+            }
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = i + 1; j < n; j++)
+                {
+                    double factor = modifiedMatrix[j][i] / modifiedMatrix[i][i];
+                    for (int k = i; k < n + 1; k++)
+                    {
+                        modifiedMatrix[j][k] -= factor * modifiedMatrix[i][k];
+                    }
+                    result[j] = string.Join(", ", modifiedMatrix[j]);
+                }
+            }
+            for (int i = n - 1; i >= 0; i--)
+            {
+                result[i] = (modifiedMatrix[i][n] / modifiedMatrix[i][i]).ToString("F2");
+                for (int j = i - 1; j >= 0; j--)
+                {
+                    modifiedMatrix[j][n] -= modifiedMatrix[j][i] * double.Parse(result[i]);
+                    result[j] = string.Join(", ", modifiedMatrix[j]);
+                }
+            }
+            return result;
+        }
+
+        public string[][] GetInfo2(double[][] matrix)
+        {
+            int n = matrix.Length;
+            string[][] results = new string[n][];
+
+            double[][] historyMatrix = new double[n][];
+            for (int i = 0; i < n; i++)
+            {
+                historyMatrix[i] = new double[matrix[i].Length];
+                matrix[i].CopyTo(historyMatrix[i], 0);
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = i + 1; j < n; j++)
+                {
+                    double factor = matrix[j][i] / matrix[i][i];
+                    for (int k = i; k < n + 1; k++)
+                    {
+                        matrix[j][k] -= factor * matrix[i][k];
+                    }
+                    results[j] = new string[n];
+                    for (int l = 0; l < n; l++)
+                    {
+                        results[j][l] = string.Join(", ", matrix[j]);
+                    }
+                }
+            }
+
+            for (int i = n - 1; i >= 0; i--)
+            {
+                results[i] = new string[n];
+                results[i][n - 1] = (matrix[i][n] / matrix[i][i]).ToString("F2");
+                for (int j = i - 1; j >= 0; j--)
+                {
+                    matrix[j][n] -= matrix[j][i] * double.Parse(results[i][n - 1]);
+                    results[j] = new string[n];
+                    for (int k = 0; k < n; k++)
+                    {
+                        results[j][k] = string.Join(", ", matrix[j]);
+                    }
+                }
+                // Store the current state of the matrix in history
+                historyMatrix[i] = new double[matrix[i].Length];
+                matrix[i].CopyTo(historyMatrix[i], 0);
+                results[i][n - 1] += ", Solution: " + results[i][n - 1];
+            }
+
+            string[][] finalResults = new string[n][];
+            for (int i = 0; i < n; i++)
+            {
+                finalResults[i] = new string[n];
+                finalResults[i] = results[i];
+            }
+
+            Console.WriteLine("History of matrix modifications:");
+            foreach (var row in historyMatrix)
+            {
+                Console.WriteLine(string.Join(", ", row));
+            }
+
+            return finalResults;
+        }
+
+
+        public List<string> GetInfo(double[][] matrix)
+        {
+            int n = matrix.Length;
+            List<string> history = new List<string> ();
+
+            double[][] historyMatrix = new double[n][];
+            for (int i = 0; i < n; i++)
+            {
+                historyMatrix[i] = new double[matrix[i].Length];
+                matrix[i].CopyTo(historyMatrix[i], 0);
+            }
+
+            for (int i = 0; i < n; i++)
+            {
+                history.Add("Matrix at step " + i + ":");
+                for (int j = 0; j < n; j++)
+                {
+                    history.Add($"Row {j}: {string.Join(", ", matrix[j])}");
+                }
+                for (int j = i + 1; j < n; j++)
+                {
+                    history.Add("Changing row " + j + ": " + string.Join(", ", matrix[j]));
+                    double factor = matrix[j][i] / matrix[i][i];
+                    for (int k = i; k < n + 1; k++)
+                    {
+                        matrix[j][k] -= factor * matrix[i][k];
+                    }
+                    history.Add("Modified row " + j + ": " + string.Join(", ", matrix[j]));
+                }
+            }
+
+            for (int i = n - 1; i >= 0; i--)
+            {
+                for (int j = i - 1; j >= 0; j--)
+                {
+                    matrix[j][n] -= matrix[j][i] * (matrix[i][n] / matrix[i][i]);
+                }
+                matrix[i][n] /= matrix[i][i];
+            }
+
+            history.Add("Matrix at final step:");
+            for (int j = 0; j < n; j++)
+            {
+                history.Add($"Row {j}: {string.Join(", ", matrix[j])}");
+            }
+            history.Add("final vector");
+            int last = matrix[0].Length-1;
+            for(int i = 0; i < n; ++i)
+            {
+                history.Add($"X {i}: {string.Join(", ", matrix[i][last])}");
+            }
+            return history;
+        }
+    }
+}
